2bc354fcfaef7799e41ce7ebb9bcca130ee838c9
Vanu Chdry <vanuchdry07@gmail.com>
2025-09-24 14:12:54 +0000

    make sure this dashboard fit according to screen (_for element <SidebarP

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -43,7 +43,7 @@ export default async function AdminDashboardPage() {
                 {stats.map(stat => <StatCard key={stat.title} {...stat} />)}
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="lg:col-span-2 space-y-6">
                     <Card>
                         <CardHeader>
@@ -62,7 +62,7 @@ export default async function AdminDashboardPage() {
                         </CardContent>
                     </Card>
                 </div>
-                <div className="space-y-6">
+                <div className="md:col-span-1 lg:col-span-1 space-y-6">
                     <Card>
                         <CardHeader>
                             <CardTitle>Quick Actions</CardTitle>
